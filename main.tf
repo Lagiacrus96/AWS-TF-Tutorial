20716ad2a0fabba3369fa270827aba492711a70f
Matthew Meinema <mattmeinema@hotmail.com>
2024-04-22 14:00:51 +0200

    Added NAT gateway. Basic network is complete.

--- main.tf
+++ main.tf
@@ -30,6 +30,11 @@ resource "aws_route_table" "public-route-table" {
     gateway_id = aws_internet_gateway.gw.id
   }
 
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
   tags = {
     Name = "Public route table"
   }
@@ -39,21 +44,42 @@ resource "aws_route_table" "public-route-table" {
 resource "aws_route_table" "private-route-table" {
   vpc_id = aws_vpc.VPC.id
 
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
+  }
+
   tags = {
     Name = "Private route table"
   }
 }
 
-# Create a security group
+# Create a public security group
 resource "aws_security_group" "public-sg" {
-  description = "Allow TLS inbound traffic and all outbound traffic"
+  description = "Allow SSH, HTTP, and HTTPS inbound traffic and all outbound traffic"
   name = "public-sg"
-  vpc_id      = aws_vpc.VPC.id
+  vpc_id = aws_vpc.VPC.id
 
   tags = {
     Name = "publicSG"
   }
 }
+# Create a security group CURRENTLY UNUSED
+resource "aws_security_group" "private-sg" {
+  description = "Allow SSH, HTTP, and HTTPS inbound traffic and all outbound traffic"
+  name = "private-sg"
+  vpc_id = aws_vpc.VPC.id
+
+  tags = {
+    Name = "privateSG"
+  }
+}
+
 # Allow inbound SSH traffic
 resource "aws_security_group_rule" "ssh_access" {
   security_group_id = aws_security_group.public-sg.id
@@ -61,6 +87,39 @@ resource "aws_security_group_rule" "ssh_access" {
   from_port          = 22
   to_port            = 22
   protocol           = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+# Allow inbound HTTP traffic
+resource "aws_security_group_rule" "http_access" {
+  security_group_id = aws_security_group.public-sg.id
+  type               = "ingress"
+  from_port          = 80
+  to_port            = 80
+  protocol           = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+# Allow inbound HTTPS traffic
+resource "aws_security_group_rule" "https_access" {
+  security_group_id = aws_security_group.public-sg.id
+  type               = "ingress"
+  from_port          = 443
+  to_port            = 443
+  protocol           = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+
+}
+
+# Allow all outbound traffic
+resource "aws_security_group_rule" "outbound_access" {
+  security_group_id = aws_security_group.public-sg.id
+  type               = "egress"
+  from_port          = 0
+  to_port            = 0
+  protocol           = -1
   cidr_blocks       = ["0.0.0.0/0"]
 
 }
@@ -126,7 +185,29 @@ resource "aws_instance" "private-instance" {
   ami = "ami-0f673487d7e5f89ca" # AWS Linux 2
   instance_type = var.instance_type
   subnet_id = aws_subnet.private-subnet.id
+  key_name = aws_key_pair.my_key_pair.key_name # TODO: Change this to another key pair
+  associate_public_ip_address = false
+  security_groups = [ aws_security_group.public-sg.id ]
   tags = {
     Name = var.private_instance_name
   }
+}
+
+# Create an EIP for the NAT gateway
+resource "aws_eip" "eip" {
+  domain   = "vpc"
+}
+
+# Create a NAT gateway
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public-subnet.id
+
+  tags = {
+    Name = "gw NAT"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
 }
